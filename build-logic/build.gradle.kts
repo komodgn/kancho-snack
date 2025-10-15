2ade57fc08d0d054cec3739d9824f5e32e1bd40c
komodgn <a01075251881@gmail.com>
2025-10-15 23:24:36 +0900

    feat: Type-Safe한 설정을 위해 Gradle 플러그인 의존성 추가 (#2)
    - build-logic의 컨벤션 플러그인에서 안드로이드 및 코틀린 프로젝트의 설정을 Type-safe하게 다루기 위해 관련 Gradle 플러그인을 compileOnly로 추가
    - 컨벤션 플러그인에서 libs 속성으로 접근 가능하도록 확장 프로퍼티 추가

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -1,13 +1,8 @@
 plugins {
-    id("java-library")
-    alias(libs.plugins.jetbrains.kotlin.jvm)
+    `kotlin-dsl`
 }
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
-kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
-    }
+
+dependencies {
+    compileOnly(libs.android.gradle.plugin)
+    compileOnly(libs.kotlin.gradle.plugin)
 }
