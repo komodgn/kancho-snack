4a5b2371974a52c2d7106bbc0df18f281f41895b
komodgn <a01075251881@gmail.com>
2025-10-20 21:06:10 +0900

    fix: 오타 수정
    - PRIVACY_POLCY -> PRIVACY_POLICY

--- core/common/src/main/java/com/komodgn/snack/core/common/constants/WebViewUrls.kt
+++ core/common/src/main/java/com/komodgn/snack/core/common/constants/WebViewUrls.kt
@@ -1,5 +1,5 @@
 package com.komodgn.snack.core.common.constants
 
 enum class WebViewUrls(val url: String) {
-    PRIVACY_POLCY("https://sites.google.com/view/snack-privacypolicy"),
+    PRIVACY_POLICY("https://sites.google.com/view/snack-privacypolicy"),
 }
