7e5b43f5161d86df8e2d02fd8c08311a857c151b
komodgn <a01075251881@gmail.com>
2025-10-22 16:18:44 +0900

    ci: 코드 스타일 체크 단계 추가 (klint, detekt)
    - 에디터 설정 추가
    - 빌드 결과 폴더 깃 추적 제외

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+import io.gitlab.arturbosch.detekt.extensions.DetektExtension
+import org.jlleitschuh.gradle.ktlint.KtlintExtension
+
 plugins {
+    alias(libs.plugins.kotlin.detekt)
+    alias(libs.plugins.kotlin.ktlint)
+
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
 
@@ -11,4 +17,30 @@ plugins {
 
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
+}
+
+val excludeModules: String? by project
+
+allprojects {
+    if (excludeModules?.split(",")?.contains(project.name) != true) {
+        apply {
+            plugin(rootProject.libs.plugins.kotlin.detekt.get().pluginId)
+            plugin(rootProject.libs.plugins.kotlin.ktlint.get().pluginId)
+        }
+    }
+
+    afterEvaluate {
+        extensions.configure<DetektExtension> {
+            parallel = true
+            buildUponDefaultConfig = true
+            toolVersion = libs.versions.kotlinDetekt.get()
+            config.setFrom(files("$rootDir/detekt-config.yml"))
+        }
+
+        extensions.configure<KtlintExtension> {
+            version.set(rootProject.libs.versions.kotlinKtlintSource.get())
+            android.set(true)
+            verbose.set(true)
+        }
+    }
 }
